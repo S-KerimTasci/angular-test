2fb8768179eb83b2bb964ca9ebecdca2fbe3c19e
Kerim <kerim.tasci@yahoo.de>
2023-11-22 21:41:43 +0100

    img slideshow

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  images = ['img1.jpg', 'img2.jpg', 'img3.jpg'];
+  currentIMG = 0;
+
+  ngOnInit() {
+    this.updateImages();
+  }
+
+  updateImages(){
+    setInterval(()=>{
+      this.currentIMG++;
+      this.currentIMG = this.currentIMG % this.images.length;
+    },5000)
+  }
+
 }
