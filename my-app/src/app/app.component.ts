d21d6afcfdb1f200fbfcb57df56fd71855d59dc5
Kerim <kerim.tasci@yahoo.de>
2023-11-28 21:21:54 +0100

    overlay and text

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   images = ['img1.jpg', 'img2.jpg', 'img3.jpg'];
+  headlines =[
+    'Bring engineering to the next level',
+    'Born to code',
+    'Another one'
+  ]
   currentIMG = 0;
+  showImage = true;
 
   ngOnInit() {
     this.updateImages();
@@ -17,6 +23,12 @@ export class AppComponent implements OnInit {
     setInterval(()=>{
       this.currentIMG++;
       this.currentIMG = this.currentIMG % this.images.length;
+      this.showImage = false;
+
+
+      setTimeout(()=>{
+        this.showImage = true;
+      },10)
     },5000)
   }
 
