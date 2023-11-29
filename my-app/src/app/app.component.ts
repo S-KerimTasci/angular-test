1066a74854561b1efb41ab94fa044e96b50336db
Kerim <kerim.tasci@yahoo.de>
2023-11-29 22:02:38 +0100

    header menu

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,35 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  images = ['img1.jpg', 'img2.jpg', 'img3.jpg'];
-  headlines =[
-    'Bring engineering to the next level',
-    'Born to code',
-    'Another one'
-  ]
-  currentIMG = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImages();
-  }
-
-  updateImages(){
-    setInterval(()=>{
-      this.currentIMG++;
-      this.currentIMG = this.currentIMG % this.images.length;
-      this.showImage = false;
-
-
-      setTimeout(()=>{
-        this.showImage = true;
-      },10)
-    },5000)
-  }
-
+export class AppComponent {
+  
 }
